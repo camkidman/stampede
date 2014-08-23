8a5250bd9d8d8c436cc1c82b00acb21d56398828
Cameron Kidman <cameronjkidman@gmail.com>
2014-08-23 17:01:21 -0600

    Stampede with postgres as a default

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Stampede::Application.config.secret_key_base = '3e3a652dd482620623e1ed6965e1481ea568d6789d28d4ac384b9b32927f8c11ba25e100c9b0521654eb470ce73b68038dd795775d45541a10809b9a01d3f2a8'
